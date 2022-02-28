24ead743ad8123ded72a8dc209ed3302f0033bfb
Craig Marshall <dev.craigmarshall@gmail.com>
2022-02-28 16:59:22 +0000

    blog page fully optimised for mobile

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -17,16 +17,16 @@ const Blog = ()=>{
                             <div className="blog-entry" key={index}>
                                 <h2 className="blog-entry-title">{item?.Title}</h2>
                                 <p className="blog-date">{item?.date}</p>
-                                <p>{item?.paragraph1}</p>
-                                <p>{item?.paragraph2}</p>
-                                <p>{item?.paragraph3}</p>
-                                <p>{item?.paragraph4}</p>
-                                <p>{item?.paragraph5}</p>
-                                <p>{item?.paragraph6}</p>
-                                <p>{item?.paragraph7}</p>
-                                <p>{item?.paragraph8}</p>
-                                <p>{item?.paragraph9}</p>
-                                <p>{item?.paragraph10}</p>
+                                <p className="blog-paragraph">{item?.paragraph1}</p>
+                                <p className="blog-paragraph">{item?.paragraph2}</p>
+                                <p className="blog-paragraph">{item?.paragraph3}</p>
+                                <p className="blog-paragraph">{item?.paragraph4}</p>
+                                <p className="blog-paragraph">{item?.paragraph5}</p>
+                                <p className="blog-paragraph">{item?.paragraph6}</p>
+                                <p className="blog-paragraph">{item?.paragraph7}</p>
+                                <p className="blog-paragraph">{item?.paragraph8}</p>
+                                <p className="blog-paragraph">{item?.paragraph9}</p>
+                                <p className="blog-paragraph">{item?.paragraph10}</p>
                             </div>
                         )})
                     }
